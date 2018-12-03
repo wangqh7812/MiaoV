f59dc4f2c9d56167c3b4cfefc64598a39064185f
duoyu <wangqh7812@163.com>
2018-12-03 20:25:47 +0800

    2018-12-03

---
+++ part_01/task_09/script.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    var oSlide = document.getElementsByClassName('slide-module');
+    var oUp = oSlide.getElementsByClassName('up');
+    var oDown = oSlide.getElementsByClassName('down');
+    var oBox = oSlide.getElementsByClassName('show-box');
+
+
+}
